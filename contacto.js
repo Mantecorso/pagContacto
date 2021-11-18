1aaf9e6a637de6a7fecf20d04329bbdf5ba4f1be
Mantecorso <luisjuradoquesada@gmail.com>
2021-11-18 18:14:28 +0100

    Validación con JS y HTML5

--- contacto.js
+++ contacto.js
@@ -11,25 +11,45 @@ window.addEventListener('load', function() {
         correo = document.getElementById('email1');
         mensaje = document.getElementById('mensaje1');
 
-        if (nombre.value == "") {
+/*         if (nombre.value == "") {
             alert('No has escrito tu nombre.');
-            // nombre.className  = 'error';
+            nombre.className  = 'error';
             nombre.focus();
-        } else if (correo.value == "") {
+        } else */ if (!nombre.checkValidity()) { // compruebo con javascript que cumple el patrón
+            alert('El nombre no cumple los requisitos.');
+            nombre.className  = 'error';
+            if (nombre.validity.valueMissing) {
+                alert("El nombre está vacio.");
+            }
+            if (nombre.validity.patternMismatch) {
+                alert("No se admiten números ni símbolos.");
+            }
+            if (nombre.validity.tooLong) {
+                alert("Nombre demasiado largo, máximo 35 caracteres.")
+            }
+        }/*  else if (correo.value == "") {
             alert('No has escrito tu correo electrónico.');
+            correo.className = "error";
             correo.focus();
+        } */ else if (!correo.checkValidity()) { // compruebo con javascript que cumple el patrón
+            alert('Formato de email erróneo.');
+            correo.className  = 'error';
+            if (correo.validity.valueMissing) {
+                alert("El email está vacio.");
+            }
+            if (correo.validity.patternMismatch) {
+                alert("Comprueba que lleve @.");
+            }
+
         } else if (mensaje.value == "") {
             alert('No has escrito ningún mensaje.');
+            mensaje.className = "error";
             mensaje.focus();
-        } else if (nombre.value != "" && correo.value != "" && mensaje.value != "" && confirm("Pulsa aceptar si quieres enviar el formulario.")) {
+        } else if (nombre.value != "" && nombre.checkValidity() && correo.value != "" && correo.checkValidity() && mensaje.value != "" && confirm("Pulsa aceptar si quieres enviar el formulario.")) {
             alert("El formulario ha sido enviado.");
         } else {
             ev.preventDefault(); //bloquea el evento submit si no cumple todos los parametros
             alert("El formulario NO ha sido enviado.");
         }
-
-        
-
     })
-
 })
